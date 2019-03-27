908ddb1581c80f26918b1e34583dcce332d14bcc
Sagi Gleizer <sagi.gleizer@gmail.com>
2019-03-27 13:50:41 +0200

    created classes components and added dev environment

---
+++ src/Details.js
@@ -0,0 +1,61 @@
+import React from "react";
+import pf from "petfinder-client";
+import Carousel from "./Carousel";
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
+
+class Details extends React.Component {
+  state = {
+    loading: true
+  };
+
+  componentDidMount() {
+    petfinder.pet
+      .get({
+        output: "full",
+        id: this.props.id
+      })
+      .then(data => {
+        const pet = data.petfinder.pet;
+        let breed;
+        breed = Array.isArray(data.petfinder.pet.breeds.breed)
+          ? data.petfinder.pet.breeds.breed.join(", ")
+          : data.petfinder.pet.breeds.breed;
+
+        this.setState({
+          name: pet.name,
+          animal: pet.animal,
+          location: `${pet.contact.city}, ${pet.contact.state}`,
+          description: pet.description,
+          media: pet.media,
+          breed,
+          loading: false
+        });
+      })
+      .catch(err => this.setState({ error: err }));
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <h1>loading...</h1>;
+    }
+    const { name, animal, breed, location, description, media } = this.state;
+    return (
+      <div className="details">
+        <Carousel media={media} />
+        <div>
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {location}
+          </h2>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Details;
